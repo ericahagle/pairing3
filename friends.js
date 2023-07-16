338d8b37e8d3176c76ef73fe5f8d0c265b2f0091
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-16 10:52:36 -0700

    Turn first loop into a function and create second function

--- friends.js
+++ friends.js
@@ -1,7 +1,27 @@
 var arrOfNums = [1, 10, 12, 55, 23, 99, 89, 2];
 
-for (var i = 0; i < arrOfNums.length; i++) {
-    if (arrOfNums[i] > 30) {
-        console.log(arrOfNums[i]);
+function numsOverThirty() {
+    var newArray = [];
+
+    for (var i = 0; i < arrOfNums.length; i++) {
+        if (arrOfNums[i] > 30) {
+            newArray.push(arrOfNums[i]);
+        }
     }
-}
+    return newArray;
+};
+
+console.log(numsOverThirty());
+
+function numsUnderSixty() {
+    var newArray = [];
+
+    for (var i = 0; i < arrOfNums.length; i++) {
+        if (arrOfNums[i] < 60) {
+            newArray.push(arrOfNums[i]);
+        }
+    }
+    return newArray;
+};
+
+console.log(numsUnderSixty());
